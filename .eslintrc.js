bdbdb1ddff369018a10c9f559ccf30f6034c812e
Xingjian <i@xingjian.me>
2017-02-19 14:31:49 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]，
+    "semi": [
+        0,
+        "never"
+    ],
+};
