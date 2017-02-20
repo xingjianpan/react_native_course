1f1ed3bd67c892fee0584cbc7b2fa8e747782beb
Xingjian <i@xingjian.me>
2017-02-20 21:47:37 +0800

    move some components to common folder

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text, View, Image, Linking } from 'react-native';
-import Card from './Card';
-import CardSection from './CardSection'
-import Button from './Button'
+import Card from './common/Card';
+import CardSection from './common/CardSection'
+import Button from './common/Button'
 
 const AlbumDetail = ({album}) => {
 
