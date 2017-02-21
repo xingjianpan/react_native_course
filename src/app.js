1b8b3d7453601339f573e2cf9cb9ecee43d613b3
xingjian <deric.pan@gmail.com>
2017-02-21 11:19:55 +0800

    login/logout using firebase

--- src/app.js
+++ src/app.js
@@ -1,31 +1,67 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import firebase from 'firebase';
+import AV from 'leancloud-storage';
+import { Header, Button, Spinner } from './components/common';
 import AlbumList from './components/AlbumList';
 import LoginForm from './components/LoginForm';
 
-// import firebase from 'firebase';
-import AV from 'leancloud-storage';
 
 const LC_APPID = 'YnPbzevdUJ5iIzvnMCXPqgRo-gzGzoHsz';
 const LC_APPKEY = 'MMs7kQ18mOLgzCqIuSFKWvD7';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // no idea whether you are logged in or not
+      loggedIn: null,
+    };
+  }
+
   componentWillMount() {
-    // firebase.initializeApp({
-    //     ''
-    //  });
-    AV.init({
-      appId: LC_APPID,
-      appKey: LC_APPKEY,
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB346_uWghszEeQuFDIGaHfoweJHJ2wHdo',
+      authDomain: 'reactnative-311d4.firebaseapp.com',
+      databaseURL: 'https://reactnative-311d4.firebaseio.com',
+      storageBucket: 'reactnative-311d4.appspot.com',
+      messagingSenderId: '973359486430',
     });
-  };
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+    // AV.init({
+    //   appId: LC_APPID,
+    //   appKey: LC_APPKEY,
+    // });
+
+  renderContent() {
+    switch (this.state.logged) {
+      case true:
+        return (
+          <Button onPress={() => { firebase.auth().SignOut(); }}>
+            Log out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
 
   render() {
     return (
-      <View style={{ flex:1 }}>
+      <View style={{ flex: 1 }}>
         <Header headerText={'Albums!!!'} />
-        <LoginForm />
+        {this.renderContent()}
         <AlbumList />
       </View>
     );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,63 @@
-import React, { Component}  from 'react';
-import { View, Text } from 'react-native';
-import { Button, Card, CardSection, Input } from './common';
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { email: '' };
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+      loading: false,
+    };
+  }
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({
+      error: '',
+      loading: true,
+    });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      // NOTE HERE NEED TO BIND THIS
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false,
+    });
+  }
+
+  onLoginFail() {
+    this.setState({
+      error: 'Authentication Failed',
+      loading: false,
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+      Login
+      </Button>
+    );
   }
 
   render() {
@@ -23,11 +75,21 @@ class LoginForm extends Component {
 
 
         <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Passwod"
+            value={this.state.password}
+            onChangeText={ password => this.setState({ password })}
+          />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
 
         <CardSection>
-          <Button>Login</Button>
+          {this.renderButton()}
         </CardSection>
 
       </Card>
@@ -36,5 +98,12 @@ class LoginForm extends Component {
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  }
+};
 
 export default LoginForm;
